824cc07c947c11716dfe0c4744e056745c01d990
TheSola10 <me@thesola.io>
2023-02-18 23:41:12 +0100

    Now actually configuring Nix sources, added store migration

--- sources/default.nix
+++ sources/default.nix
@@ -9,11 +9,27 @@ let
     ];
   srcs_dir =
     [ nlohmann_json
-      nix
       # I want pname to match the pkg-config package checked against like in the script
       (brotli.overrideAttrs (_: { pname = "libbrotlicommon"; }))
       (editline.overrideAttrs (_: { pname = "libeditline"; }))
     ];
+
+  nix_configured_src = stdenv.mkDerivation {
+    inherit (nix) version src nativeBuildInputs;
+    pname = "nix-configured-source";
+
+    configurePhase = ''
+      ./bootstrap.sh
+    '';
+
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r $src $out/nix
+      chmod -R u+w $out/nix
+    '';
+  };
 in stdenv.mkDerivation {
   pname = "nix-wrap-sources";
   version = "0.0.1";
@@ -32,6 +48,7 @@ in stdenv.mkDerivation {
     mkdir -p $out
     make BOOST_ARCHIVE=${boost.src} BOOST_VER=${boost.version} TAR=${gnutar}/bin/tar
     cp boost-shaved.tar.gz $out/boost.tar.gz
+    tar -C ${nix_configured_src} -czf $out/nix.tar.gz nix
   ''
   + builtins.foldl'
       (l: r: l + "\npython3 ./tarmod.py ${r.src} $out/${r.pname}.tar.gz ${r.pname}") "" srcs_simple
