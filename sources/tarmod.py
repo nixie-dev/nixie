e2d9c078ecf931a98cc8b9577c74d169d39c3b40
TheSola10 <me@thesola.io>
2023-02-18 22:23:50 +0100

    Fixed tarmod

--- sources/tarmod.py
+++ sources/tarmod.py
@@ -2,11 +2,12 @@
 
 import tarfile as tf
 import sys
-from re import sub
+from copy import deepcopy
+from re   import sub
 
 with tf.open(sys.argv[1], mode='r:gz') as m:
     with tf.open(sys.argv[2], mode='x:gz') as n:
         for fs in m.getmembers():
-            n.addfile(tf.TarInfo(
-                sub("^[a-zA-Z0-9-.]*/", "%s/" %sys.argv[3], fs.name)
-            ), m.extractfile(fs))
+            newfs = deepcopy(fs)
+            newfs.name = sub("^[a-zA-Z0-9-.]*/", "%s/" %sys.argv[3], fs.name)
+            n.addfile(newfs, m.extractfile(fs))
