417627bec3b49926327f05e51fec2ab2fa89e377
TheSola10 <me@thesola.io>
2023-02-19 11:04:51 +0100

    Changed configure strategy for sources

--- sources/default.nix
+++ sources/default.nix
@@ -2,6 +2,36 @@
 , gnutar, coreutils, findutils, python3, nix, ... }:
 
 let
+
+
+  mkConfiguredSrc = { pkg, confScript, dest?pkg.pname }:
+    stdenv.mkDerivation {
+      inherit (pkg) version src nativeBuildInputs;
+      inherit dest;
+      pname = "${pkg.pname}-configured-sources";
+
+      configurePhase = confScript;
+
+      dontBuild = true;
+
+      installPhase = ''
+        mkdir -p $out
+        cp -r $src $out/${dest}
+      '';
+    };
+
+    nix_configured_src = mkConfiguredSrc { pkg = nix; confScript = "./bootstrap.sh"; };
+    editline_configured_src = mkConfiguredSrc
+      { pkg = editline;
+        confScript = "./autogen.sh";
+        dest = "libeditline";
+      };
+    brotli_configured_src = mkConfiguredSrc
+      { pkg = brotli;
+        confScript = "./bootstrap";
+        dest = "libbrotlicommon";
+      };
+
   srcs_simple =
     [ openssl
       lowdown
@@ -9,41 +39,12 @@ let
     ];
   srcs_dir =
     [ nlohmann_json
-      # I want pname to match the pkg-config package checked against like in the script
-      (brotli.overrideAttrs (_: { pname = "libbrotlicommon"; }))
     ];
-
-  nix_configured_src = stdenv.mkDerivation {
-    inherit (nix) version src nativeBuildInputs;
-    pname = "nix-configured-source";
-
-    configurePhase = ''
-      ./bootstrap.sh
-    '';
-
-    dontBuild = true;
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r $src $out/nix
-    '';
-  };
-
-  editline_configured_src = stdenv.mkDerivation {
-    inherit (editline) version src nativeBuildInputs;
-    pname = "editline-configured-source";
-
-    configurePhase = ''
-      ./autogen.sh
-    '';
-
-    dontBuild = true;
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r $src $out/libeditline
-    '';
-  };
+  srcs_configured =
+    [ nix_configured_src
+      editline_configured_src
+      brotli_configured_src
+    ];
 in stdenv.mkDerivation {
   pname = "nix-wrap-sources";
   version = "0.0.1";
@@ -62,17 +63,12 @@ in stdenv.mkDerivation {
     mkdir -p $out
     make BOOST_ARCHIVE=${boost.src} BOOST_VER=${boost.version} TAR=${gnutar}/bin/tar
     cp boost-shaved.tar.gz $out/boost.tar.gz
-    cp -r ${nix_configured_src} nixsrc
-    chmod -R u+w nixsrc
-    tar -C nixsrc -czf $out/nix.tar.gz nix
-
-    cp -r ${editline_configured_src} elsrc
-    chmod -R u+w elsrc
-    tar -C elsrc -czf $out/libeditline.tar.gz libeditline
   ''
   + builtins.foldl'
       (l: r: l + "\npython3 ./tarmod.py ${r.src} $out/${r.pname}.tar.gz ${r.pname}") "" srcs_simple
   + builtins.foldl'
       (l: r: l + "\ncp -r ${r.src} ${r.pname} && chmod -R u+w ${r.pname} && tar -czf $out/${r.pname}.tar.gz ${r.pname}") "" srcs_dir
+  + builtins.foldl'
+      (l: r: l + "\ncp -r ${r}/${r.dest} work && chmod -R u+w work/${r.dest} && tar -C work -czf $out/${r.dest}.tar.gz ${r.dest}") "" srcs_configured
   ;
 }
